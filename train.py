f7859c5542c3747795e3d75d95338ee2472882cb
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2025-10-01 11:19:53 -0400

    Added validation loss logging

--- train.py
+++ train.py
@@ -30,8 +30,9 @@ else:
     print("No existing checkpoint found, starting with fresh model.")
     saver = tf.train.Saver(write_version = saver_pb2.SaverDef.V2)
 
-# create a summary to monitor cost tensor
-tf.summary.scalar("loss", loss)
+# create summaries to monitor training and validation loss tensors
+train_loss_summary = tf.summary.scalar("train_loss", loss)
+val_loss_summary = tf.summary.scalar("val_loss", loss)
 # merge all summaries into a single op
 merged_summary_op = tf.summary.merge_all()
 
@@ -47,14 +48,16 @@ for epoch in range(epochs):
   for i in range(int(driving_data.num_images/batch_size)):
     xs, ys = driving_data.LoadTrainBatch(batch_size)
     train_step.run(feed_dict={model.x: xs, model.y_: ys, model.keep_prob: 0.8})
-    if i % 10 == 0:
-      xs, ys = driving_data.LoadValBatch(batch_size)
-      loss_value = loss.eval(feed_dict={model.x:xs, model.y_: ys, model.keep_prob: 1.0})
-      print("Epoch: %d, Step: %d, Loss: %g" % (epoch, epoch * batch_size + i, loss_value))
 
-    # write logs at every iteration
-    summary = merged_summary_op.eval(feed_dict={model.x:xs, model.y_: ys, model.keep_prob: 1.0})
-    summary_writer.add_summary(summary, epoch * driving_data.num_images/batch_size + i)
+    # compute and log training loss
+    train_loss_value, train_loss_summ = sess.run([loss, train_loss_summary], feed_dict={model.x: xs, model.y_: ys, model.keep_prob: 1.0})
+    summary_writer.add_summary(train_loss_summ, epoch * driving_data.num_images/batch_size + i)
+
+    if i % 10 == 0:
+      xs_val, ys_val = driving_data.LoadValBatch(batch_size)
+      val_loss_value, val_loss_summ = sess.run([loss, val_loss_summary], feed_dict={model.x: xs_val, model.y_: ys_val, model.keep_prob: 1.0})
+      print("Epoch: %d, Step: %d, Train Loss: %g, Val Loss: %g" % (epoch, epoch * batch_size + i, train_loss_value, val_loss_value))
+      summary_writer.add_summary(val_loss_summ, epoch * driving_data.num_images/batch_size + i)
 
     if i % batch_size == 0:
       if not os.path.exists(LOGDIR):
